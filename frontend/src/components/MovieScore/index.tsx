c2e7984421a89b535076f8ee2f90ee8a592a2b08
Sandro Ribeiro <sandroccribeiro@gmail.com>
2022-03-08 19:51:16 -0800

    Movie card

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,18 @@
+
+import MovieStars from 'components/MovieStars';
+import './styles.css'
+
+const score = 5.2;
+const count = 13;
+
+function MovieScore(){
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+            <MovieStars />
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    );
+}
+
+export default MovieScore;
